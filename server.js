4c23a3eb358ca914299f548e94bba3429cd0d90f
Jason Cooper <jasonacooper@gmail.com>
2021-01-25 16:27:28 -0500

    Update navbar image and set favicon

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const fs = require('fs');
 const _sample = require('lodash/sample');
+var path = require('path');
+var favicon = require('serve-favicon');
 
 const app = express();
 
 app.set('port', process.env.PORT);
 app.set('view engine', 'ejs');
 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.get('/', (req, res) => {
-	fs.readdir('./public/images/hero', { withFileTypes: true }, (err, dirents) => {
+	fs.readdir(path.join(__dirname, 'public', 'images', 'hero'), { withFileTypes: true }, (err, dirents) => {
 		const heroFileNames = dirents.reduce((memo, dirent) => {
 			if (dirent.isFile()) {
 				memo.push(dirent.name);
@@ -18,7 +22,7 @@ app.get('/', (req, res) => {
 			return memo;
 		}, []);
 
-		res.render('pages/index', {
+		res.render(path.join('pages', 'index'), {
 			hero: _sample(heroFileNames)
 		});
 	});
