f5a9c7b8c298a1b3a9aa3ca1f0bb9ec48a59ae4d
Jason Cooper <jasonacooper@gmail.com>
2021-01-25 14:47:40 -0500

    Switch out static HTML for EJS template

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const express = require('express');
 const app = express();
 
 app.set('port', process.env.PORT);
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+	res.render('pages/index');
+});
 
 app.use(express.static('public'));
 
