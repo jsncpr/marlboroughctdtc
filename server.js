facf489e8ec999667dfa1b0bf9a7ecf2a4676311
Jason Cooper <jasonacooper@gmail.com>
2021-04-11 13:21:51 -0400

    Wire up /feedback response handler to send email via Mailgun

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
+const axios = require('axios');
 const bodyParser = require('body-parser');
 const express = require('express');
 const fs = require('fs');
 const _sample = require('lodash/sample');
-var path = require('path');
-var favicon = require('serve-favicon');
+const path = require('path');
+const qs = require('qs');
+const favicon = require('serve-favicon');
 
 const app = express();
 
@@ -35,9 +37,28 @@ app.get('/latest', (req, res) => {
 });
 
 app.post('/feedback', (req, res) => {
-	// const { email, message } = req.body;
+	const { email, message } = req.body;
+	let domain = process.env.MAILGUN_DOMAIN;
+	let to = process.env.MAILGUN_TO;
 
-	res.end();
+	if (process.env.NODE_ENV === 'development') {
+		domain = process.env.MAILGUN_DOMAIN_SANDBOX;
+		to = process.env.MAILGUN_TO_SANDBOX;
+	}
+
+	axios.post(`https://api:${process.env.MAILGUN_API_KEY}@api.mailgun.net/v2/${domain}/messages`, qs.stringify({
+		from: email,
+		subject: 'MDTC website contact form submission',
+		text: message,
+		to
+	}))
+		.catch(error => {
+			res.status(500);
+			console.log(error);
+		})
+		.finally(() => {
+			res.end();
+		});
 });
 
 app.use(express.static('public'));
